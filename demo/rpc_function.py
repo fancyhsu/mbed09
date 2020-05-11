3439ddc9bc3a2aee86abd2250ee7e894ba48b7bb
fancyhsu <fancy27829344@gmail.com>
2020-05-11 22:47:11 +0800

    2

--- demo/rpc_function.py
+++ demo/rpc_function.py
@@ -37,5 +37,5 @@ while(1):
     print(line)
     line=s.readline() # Read an echo string from K66F terminated with '\n' (RPC reply)
     print(line)
-    time.sleep(1)
+    time.sleep(1)fa
 s.close()
